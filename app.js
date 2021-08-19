d68741c72131d5491cd9e80a5c9e107b1b0a31c4
DarshanCodehunt <darshankodkany@gmail.com>
2021-08-19 13:34:34 +0530

    Update app.js
    Any change

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 window.addEventListener("load", function () {
-  console.log("Hello World! New Data Store added");
+  console.log("Hello World! New Data Store added11111");
 });
+
+const element = document.getElementById('datastore-value');
+
+const store = navigator.getDataStores("dummy_store");
+console.log('Store',store);
+
+store.then(function (stores) {
+  stores[0].get(221).then(function (response) {
+    console.log(response)
+     element.innerText = response;
+  });
+});
+
 
 document.addEventListener("keydown", handleevent);
 
